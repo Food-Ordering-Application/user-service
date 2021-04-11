806eeb00bfb9042ccf6dab0a839dc5c1d9e8c416
ADMINAdmin <thachdau16t@gmail.com>
2021-04-11 17:10:38 +0700

    Update automate build

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  // const config = app.get<ConfigService>(ConfigService);
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.RMQ,
+  //   options: {
+  //     urls: [config.get('AMWP_URL')],
+  //     queue: config.get('AMWP_QUEUE'),
+  //     queueOptions: {
+  //       durable: false,
+  //     },
+  //   },
+  // });
+  // app.startAllMicroservices(() => Logger.log('Microservice has started...'));
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://admin:admin@localhost:5672'],
+        urls: ['amqp://admin:admin@rabbitmq:5672'],
         queue: 'cats_queue',
         queueOptions: {
           durable: false,
