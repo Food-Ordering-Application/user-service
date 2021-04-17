a66a1ebbcc61d0f7023fe6cbc4ec1acde982b89f
ADMINAdmin <thachdau16t@gmail.com>
2021-04-17 13:18:45 +0700

    create entities for restaurant service

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,6 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  // const config = app.get<ConfigService>(ConfigService);
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.RMQ,
-  //   options: {
-  //     urls: [config.get('AMWP_URL')],
-  //     queue: config.get('AMWP_QUEUE'),
-  //     queueOptions: {
-  //       durable: false,
-  //     },
-  //   },
-  // });
-  // app.startAllMicroservices(() => Logger.log('Microservice has started...'));
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
