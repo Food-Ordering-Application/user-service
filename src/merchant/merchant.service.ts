bbbae4e44fe5c3ec78814459e638b6a5ffd8d780
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-19 18:28:17 +0700

    add create merchant service

--- src/merchant/merchant.service.ts
+++ src/merchant/merchant.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { MerchantDto } from './dto/merchant.dto';
+import { Merchant } from './entities/merchant.entity';
+import { HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateMerchantDto } from './dto/create-merchant.dto';
 import { UpdateMerchantDto } from './dto/update-merchant.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MerchantService {
-  create(createMerchantDto: CreateMerchantDto) {
-    return 'This action adds a new merchant';
+  private readonly logger = new Logger('CustomerService');
+
+  constructor(
+    @InjectRepository(Merchant)
+    private merchantsRepository: Repository<Merchant>,
+  ) { }
+
+  async create(createMerchantDto: CreateMerchantDto) {
+    const { username, password, email, phone, fullName, IDNumber } = createMerchantDto;
+    const merchantWithThisUsername = await this.merchantsRepository.findOne({
+      username
+    });
+    if (merchantWithThisUsername) {
+      return {
+        status: HttpStatus.CONFLICT,
+        message: 'Username already exists',
+        user: null,
+      }
+    }
+    const newUser = this.merchantsRepository.create({
+      username, password, email, phone, fullName, IDNumber
+    });
+    await this.merchantsRepository.save(newUser);
+    return {
+      status: HttpStatus.CREATED,
+      message: 'User created successfully',
+      user: MerchantDto.EntityToDTO(newUser),
+    };
   }
 
-  findAll() {
-    return `This action returns all merchant`;
-  }
+  // findAll() {
+  //   return `This action returns all merchant`;
+  // }
 
-  findOne(id: number) {
-    return `This action returns a #${id} merchant`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} merchant`;
+  // }
 
-  update(id: number, updateMerchantDto: UpdateMerchantDto) {
-    return `This action updates a #${id} merchant`;
-  }
+  // update(id: number, updateMerchantDto: UpdateMerchantDto) {
+  //   return `This action updates a #${id} merchant`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} merchant`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} merchant`;
+  // }
 }
