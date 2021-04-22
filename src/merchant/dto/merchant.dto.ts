97ca027c73b35e4f60ceffb435fddd592913bae0
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-22 13:58:41 +0700

    remove password from merchant dto

--- src/merchant/dto/merchant.dto.ts
+++ src/merchant/dto/merchant.dto.ts
@@ -3,15 +3,14 @@ import { Merchant } from "../entities/merchant.entity";
 export class MerchantDto {
   id: string;
   username: string;
-  password: string;
   email: string;
   phone: string;
   fullName: string;
   IDNumber: string;
   static EntityToDTO(merchant: Merchant): MerchantDto {
-    const { id, username, password, email, phone, fullName, IDNumber } = merchant;
+    const { id, username, email, phone, fullName, IDNumber } = merchant;
     return {
-      id, username, password, email, phone, fullName, IDNumber
+      id, username, email, phone, fullName, IDNumber
     };
   }
 }
