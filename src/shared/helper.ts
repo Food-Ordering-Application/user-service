2e066ba685f2032138d00b60aefaade292c97895
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-22 14:23:48 +0700

    fix compareHashedPassword return type

--- src/shared/helper.ts
+++ src/shared/helper.ts
@@ -3,3 +3,7 @@ import * as bcrypt from 'bcrypt';
 export async function hash(input): Promise<string> {
   return bcrypt.hash(input, 12);
 }
+
+export async function validateHashedPassword(rawPassword: string, hashedPassword: string): Promise<boolean> {
+  return await bcrypt.compare(rawPassword, hashedPassword);
+}
