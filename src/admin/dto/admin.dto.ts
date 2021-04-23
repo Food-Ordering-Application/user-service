702246531c6570ef3cd31288e675f58eadfb8c7c
mrjared16 <doannguyentanphuc99@gmail.com>
2021-04-23 18:17:06 +0700

    add admin dto for admin to user service

---
+++ src/admin/dto/admin.dto.ts
@@ -0,0 +1,13 @@
+import { Admin } from "../entities/admin.entity";
+
+export class AdminDto {
+  id: string;
+  username: string;
+  name: string;
+  static EntityToDTO(admin: Admin): AdminDto {
+    const { id, username, name } = admin;
+    return {
+      id, username, name
+    };
+  }
+}
