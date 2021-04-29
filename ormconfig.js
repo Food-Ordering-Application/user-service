d13213796031c97154d23d02b91805a64bdf991d
Van Hiep <levanhiep.95.95@gmail.com>
2021-04-30 00:57:37 +0700

    config db:setup

--- ormconfig.js
+++ ormconfig.js
@@ -1,13 +1,19 @@
 module.exports = {
   type: 'postgres',
-  host: 'postgis',
-  port: '5432',
-  username: 'admin',
-  password: 'admin',
-  database: 'userservice',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [__dirname + '/**/*.entity.{js,ts}'],
   migrations: [__dirname + '/**/*.migration.{js,ts}'],
   synchronize: true,
   seeds: [__dirname + '/**/seeders/**/*.seed{.ts,.js}'],
   factories: [__dirname + '/**/factories/**/*.factory{.ts,.js}'],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
