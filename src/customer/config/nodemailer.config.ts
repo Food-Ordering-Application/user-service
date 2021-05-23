3a26f5f8a905b3638a6ccdbb68315db7e045089a
ADMIN\Admin <thachdau16t@gmail.com>
2021-05-23 14:08:26 +0700

    Update nodemailer.config.ts

--- src/customer/config/nodemailer.config.ts
+++ src/customer/config/nodemailer.config.ts
@@ -1,5 +1,8 @@
 import * as nodemailer from 'nodemailer';
 
+console.log(process.env.HOST_EMAIL);
+console.log(process.env.HOST_EMAIL_PASSWORD);
+
 export const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
