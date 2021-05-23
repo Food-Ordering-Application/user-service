2eaa8bd8d7fa2be7b08479a341ee24f2aad67274
ADMIN\Admin <thachdau16t@gmail.com>
2021-05-23 12:24:02 +0700

    push to debug

--- src/customer/config/nodemailer.config.ts
+++ src/customer/config/nodemailer.config.ts
@@ -1,8 +1,9 @@
 import * as nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'smtp.gmail.com',
   auth: {
+    type: 'login',
     user: process.env.HOST_EMAIL,
     pass: process.env.HOST_EMAIL_PASSWORD,
   },
